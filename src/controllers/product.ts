12c1804bf96fee060d6a73a7baa05b38129be3ba
matheuskdr <matheusgabriellkpo@gmail.com>
2025-12-04 10:29:34 -0300

    Get produc with images route created

--- src/controllers/product.ts
+++ src/controllers/product.ts
@@ -1,7 +1,14 @@
 import { RequestHandler } from "express";
 import { getProductSchema } from "../schemas/get-product-schema";
-import { getAllProducts } from "../services/product";
+import {
+    getAllProducts,
+    getProduct,
+    incrementProductView,
+} from "../services/product";
 import { getAbsoluteImageUrl } from "../utils/get-absolute-image-urll";
+import { error } from "node:console";
+import { getOneProductSchema } from "../schemas/get-one-produc-schema";
+import { getCategory } from "../services/category";
 
 export const getProducts: RequestHandler = async (req, res) => {
     const parseResult = getProductSchema.safeParse(req.query);
@@ -27,4 +34,30 @@ export const getProducts: RequestHandler = async (req, res) => {
     }));
 
     res.json({ error: null, products: productsWithAbsoluteUrl });
+};
+
+export const getOneProduct: RequestHandler = async (req, res) => {
+    const paramsResult = getOneProductSchema.safeParse(req.params);
+    if (!paramsResult.success) {
+        res.status(400).json({ error: "Parâmetros inválidos" });
+        return;
+    }
+    const { id } = paramsResult.data;
+
+    const product = await getProduct(parseInt(id));
+    if (!product) {
+        res.json({ error: "Produto não encontrado" });
+        return;
+    }
+
+    const productsWithAbsoluteImages = {
+        ...product,
+        images: product.images.map((img) => getAbsoluteImageUrl(img)),
+    };
+
+    const category = await getCategory(product.categoryId);
+
+    await incrementProductView(product.id);
+
+    res.json({ error: null, product: productsWithAbsoluteImages, category });
 };
