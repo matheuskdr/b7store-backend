e056ce07f0403e3147f630a8a519ca179cd59363
matheuskdr <matheusgabriellkpo@gmail.com>
2025-12-05 14:29:31 -0300

    fix seed and images

--- src/controllers/order.ts
+++ src/controllers/order.ts
@@ -1,6 +1,9 @@
 import { RequestHandler } from "express";
 import { getOrderBySessionIdSchema } from "../schemas/get-order-by-session-id";
 import { getOrderIdFromSession } from "../services/payment";
+import { getOrderById, getUserOrders } from "../services/order";
+import { getOrderSchema } from "../schemas/get-order-schema";
+import { getAbsoluteImageUrl } from "../utils/get-absolute-image-urll";
 
 export const getOrderBySessionId: RequestHandler = async (req, res) => {
     const result = getOrderBySessionIdSchema.safeParse(req.query);
@@ -17,4 +20,56 @@ export const getOrderBySessionId: RequestHandler = async (req, res) => {
     }
 
     res.json({ error: null, orderId });
+};
+
+export const listOrders: RequestHandler = async (req, res) => {
+    const userId = (req as any).userId;
+    if (!userId) {
+        res.status(401).json({ error: "Acesso negado" });
+        return;
+    }
+
+    const orders = await getUserOrders(userId);
+
+    res.json({ error: null, orders });
+};
+
+export const getOrder: RequestHandler = async (req, res) => {
+    const userId = (req as any).userId;
+    if (!userId) {
+        res.status(401).json({ error: "Acesso negado" });
+        return;
+    }
+
+    const result = getOrderSchema.safeParse(req.params);
+    if (!result.success) {
+        res.status(401).json({ error: "ID inválido" });
+        return;
+    }
+
+    const { id } = result.data;
+
+    const order = await getOrderById(parseInt(id), userId);
+    if (!order) {
+        res.status(404).json({ error: "Pedido não encontrado" });
+        return;
+    }
+
+    const itemsWithAbsoluteUrl = order.orderItems.map((item) => ({
+        ...item,
+        product: {
+            ...item.product,
+            image: item.product.image
+                ? getAbsoluteImageUrl(item.product.image)
+                : null,
+        },
+    }));
+
+    res.json({
+        error: null,
+        order: {
+            ...order,
+            orderItems: itemsWithAbsoluteUrl,
+        },
+    });
 };
